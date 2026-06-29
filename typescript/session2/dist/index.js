c03e20c2858832cabccc572d1dd4c264788a058c
MITHULA_M_P <mithula395@gmail.com>
2026-06-29 10:05:06 +0530

    setup typescript session2

---
+++ typescript/session2/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+const studentName = "Mithula";
+const score = 95;
+function displayResult(name, marks) {
+    return `${name} scored ${marks} marks`;
+}
+console.log(displayResult(studentName, score));
